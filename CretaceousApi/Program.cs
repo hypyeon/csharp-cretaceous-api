d81324c4c89a52e5b005cfc713ee7f886c978cc7
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-21 15:47:17 -0700

    update .csproj, dev appsettings, Program.cs if, and localhost in .json

---
+++ CretaceousApi/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// enabling Swagger doc to proceed, exposing API's endpoint for docs 
+// its tools will be sued in order for developers to control what endpoints to be visible to Swagger doc 
+builder.Services.AddSwaggerGen();
+// Swagger: a service that authomatically documents the available endpoints in application 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else 
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

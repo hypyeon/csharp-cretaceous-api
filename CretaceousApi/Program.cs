4cef0626e2bf880d12b18ea4bde4dd7af260fe88
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-22 15:38:04 -0700

    delete WeatherForecast and add model and database for animal class

--- CretaceousApi/Program.cs
+++ CretaceousApi/Program.cs
@@ -1,9 +1,19 @@
+using CretaceousApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<CretaceousApiContext>(
+    DbContextOptions => DbContextOptions.UseMySql(
+        builder.Configuration["ConnectionStrings:DefaultConnection"],
+        ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+        )
+    )
+);
 builder.Services.AddEndpointsApiExplorer();
 // enabling Swagger doc to proceed, exposing API's endpoint for docs 
 // its tools will be sued in order for developers to control what endpoints to be visible to Swagger doc 
